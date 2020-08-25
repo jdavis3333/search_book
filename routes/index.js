a6e8751e2acc2cba1fba6829a49aa0bbc3730ce9
jdavis3333 <jdavis3333@gmail.com>
2020-08-25 11:36:16 -0700

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const apiRoutes = require("./api");
 
 // API Routes
 router.use("/api", apiRoutes);
-router.use("/" ,(req, res)=>{
-    res.send("Meow Woof")
-})
+// router.use("/" ,(req, res)=>{
+//     res.send("Meow Woof")
+// })
 
 // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
